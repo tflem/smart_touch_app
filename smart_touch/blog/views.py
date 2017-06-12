04ef7b9bb200dc253e0a7296acd746aec76301ef
Tim Fleming <tfleming26@hotmail.com>
2017-06-11 22:43:28 -0600

    Add pagination to blog

--- smart_touch/blog/views.py
+++ smart_touch/blog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     """Retrieves all posts witin published status using manager"""
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page not integer, deliver first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range, deliver last page
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page,
+                  'posts': posts})
 
 def post_detail(request, year, month, day, post):
     """Retrieves post data with given parameters."""
